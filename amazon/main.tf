e2d95a8b4234395e53c53974322894e6f238613c
grigorenko <grigorenko@hpe.com>
2023-09-29 11:06:35 +0300

    update

--- amazon/main.tf
+++ amazon/main.tf
@@ -6,11 +6,10 @@ provider "aws" {
 resource "aws_instance" "rhel_test_vm" {
   ami           = "ami-0874ff0d73a3ab8cf"
   instance_type = "t3.micro"
-  user_data     = "admin"
-
   tags = {
-    Name = "test_VM"
+    Name = "Test VM"
   }
-
 }
+
+
 
