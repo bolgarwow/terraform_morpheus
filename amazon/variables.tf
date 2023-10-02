b3116567f3e26d044f12f72719b965c1cdab2306
grigorenko <grigorenko@hpe.com>
2023-10-02 09:43:41 +0300

    update

--- amazon/variables.tf
+++ amazon/variables.tf
@@ -6,3 +6,9 @@ variable "aws_secret_key" {
   type      = string
   sensitive = true
 }
+variable "nsg_default" {
+  default = "sg-037fd92538c01600d"
+}
+variable "key_name" {
+  default = "sshconnecttion"
+}
